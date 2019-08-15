1ce3ae6a550472a7382df92e447ff014f0e42e03
akoan60 <anaelakue60@gmail.com>
2019-08-15 16:58:59 +0200

    Mexican wave exercice added

---
+++ mexicanWave.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void mexicanWave(char *sample, char **target)
+{
+	
+}
+
+//void printArray(char** theArray)
+//{
+//	for (int i = 0; i < strlen(theArray); i++) 
+//	{
+//		for (int j = 0; j < strlen(theArray); j++) 
+//		{
+//			printf("%c\n ", theArray[i][j]);
+//		}
+//		printf("\n");
+//	}
+//}
